567f8902be5ec1cebd57ad5b6571014ec092d6f5
meetmistry28 <meetmistry7007@gmail.com>
2025-11-12 14:51:22 +0530

    1.1.1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async';
 // import { HelmetProvider } from 'react-helmet';
 
 // Add Helmet provider for better SSR support
@@ -10,8 +9,6 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </StrictMode>
 );
